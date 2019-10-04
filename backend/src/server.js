b4b138562227b0f2522f68b64f74394938f6a8dd
cristianldesouza <cristianldesouza@hotmail.com>
2019-10-03 22:45:14 -0300

    frontend finalizado

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -12,6 +13,7 @@ mongoose.connect('mongodb+srv://aircnc:aircnc@aircnc-mtu6h.mongodb.net/aircnc?re
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 
